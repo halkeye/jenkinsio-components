213b1b982634b25ee931e0accfab51555bc4fe56
Gavin Mogan <git@gavinmogan.com>
2022-05-29 12:04:47 -0700

    sass support

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,7 +1,11 @@
-import { Config } from '@stencil/core';
+import {Config} from '@stencil/core';
+import {sass} from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'jenkinsio-components',
+  plugins: [
+    sass()
+  ],
   outputTargets: [
     {
       type: 'dist',
